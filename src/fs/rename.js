a315abc6400cc48f4c2e0c4f57e04c6896c36079
12FourOnSix <fouronsix@yandex.ru>
2022-06-05 02:27:59 +0300

    feat: add solution for fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,21 @@
+import { rename as renameFile } from 'fs/promises';
+import { existsSync } from 'fs';
+
+const 
+    wrongFilenamePath = './src/fs/files/wrongFilename.txt',
+    properFilenamePath = './src/fs/files/properFilename.md';
+
 export const rename = async () => {
-    // Write your code here 
+    if (!existsSync(wrongFilenamePath) || existsSync(properFilenamePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    await renameFile(wrongFilenamePath, properFilenamePath);
 };
+
+
+try {
+    rename();
+} catch (err) {
+    console.error(err);
+}
