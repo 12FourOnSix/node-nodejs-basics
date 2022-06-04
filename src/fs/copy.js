945960f812f7978c3a01939e9f2778869111afe0
12FourOnSix <fouronsix@yandex.ru>
2022-06-05 02:18:52 +0300

    feat: add solution for fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,31 @@
+import { copyFile, readdir, mkdir } from 'fs/promises';
+import { existsSync } from 'fs';
+import { resolve } from 'path';
+
+const 
+    origFolderPath = './src/fs/files',
+    destFolderPath = './src/fs/files_copy';
+
 export const copy = async () => {
-    // Write your code here 
+    if (!existsSync(origFolderPath) || existsSync(destFolderPath)) {
+        throw new Error('FS operation failed');
+    }
+
+    await mkdir(destFolderPath);
+    const files = await readdir(origFolderPath);
+
+    for await (const filename of files) {
+        const 
+            origFilePath = resolve(origFolderPath, filename),
+            destFilePath = resolve(destFolderPath, filename);
+
+        copyFile(origFilePath, destFilePath);
+    }
 };
+
+
+try {
+    copy();
+} catch (err) {
+    console.error(err);
+}
