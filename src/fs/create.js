37132070f909e33503619719bda6c9a65229809b
12FourOnSix <fouronsix@yandex.ru>
2022-06-05 00:31:59 +0300

    feat: add solution for fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import { appendFile } from 'fs/promises';
+import { existsSync } from 'fs';
+
+const 
+    filePath = './src/fs/files/fresh.txt',
+    text = 'I am fresh and young';
+
+
 export const create = async () => {
-    // Write your code here 
+    if (existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+    await appendFile(filePath, text);
 };
+
+
+try {
+    create();
+} catch (err) {
+    console.error(err);
+}
