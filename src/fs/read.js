1236b9d52d2dd23e5c0c52a4c059fea4c2a98985
12FourOnSix <fouronsix@yandex.ru>
2022-06-05 02:46:51 +0300

    feat: add solution for fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import { readFile } from 'fs/promises';
+import { existsSync } from 'fs';
+
+const fileToReadPath = './src/fs/files/fileToRead.txt';
+
 export const read = async () => {
-    // Write your code here 
+    if (!existsSync(fileToReadPath)) throw new Error('FS operation failed');
+
+    const fileContent = await readFile(fileToReadPath, { encoding: 'utf-8' });
+    console.log(fileContent);
 };
+
+
+try {
+    read();
+} catch (err) {
+    console.error(err);
+}
