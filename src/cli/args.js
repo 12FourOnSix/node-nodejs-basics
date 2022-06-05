d3eb95155a249051a4cda6882bcc459edd45ddda
12FourOnSix <fouronsix@yandex.ru>
2022-06-05 04:14:28 +0300

    feat: add solution for cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,30 @@
+import { argv } from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+  const argsArr = argv.slice(2);
+  let resultStr = '';
+
+  for (const item of argsArr) {
+    const isEven = argsArr.indexOf(item) % 2 === 0;
+    let propName, propValue;
+
+    if (isEven) {
+      propName = item.replace('--', '');
+      resultStr += propName;
+    } else {
+      propValue = ` is ${item}, `;
+      resultStr += propValue;
+    }
+  }
+
+  if (resultStr.length === 0) throw new Error('Не передано ни одного аргумента');
+  
+  resultStr = resultStr.trim().replace(/\,$/, '');
+  console.log(resultStr);
 };
+
+try {
+  parseArgs();
+} catch (err) {
+  console.error(err);
+}
