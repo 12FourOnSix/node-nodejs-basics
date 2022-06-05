93867ed563e5e1d764ba40e99ae1cee0bad60fb7
12FourOnSix <fouronsix@yandex.ru>
2022-06-05 21:59:05 +0300

    feat: add solution for zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,24 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+
+const 
+  fileToZipPath = './src/zip/files/fileToCompress.txt',
+  zippedFilePath = './src/zip/files/archive.gz',
+  readStream = createReadStream(fileToZipPath),
+  writeStream = createWriteStream(zippedFilePath),
+  gzip = createGzip();
+
 export const compress = async () => {
-    // Write your code here 
+  try {
+    await pipeline(
+      readStream, 
+      gzip, 
+      writeStream
+    );
+  } catch (err) {
+    console.error(err);
+  }
 };
+
+compress();
