45e4283b0d3ee77219114359fa01fb68eb8477e1
12FourOnSix <fouronsix@yandex.ru>
2022-06-05 21:19:39 +0300

    feat: add solution for streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,30 @@
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
+const reverseText = new Transform({
+  transform(chunk, encoding, callback) {
+    const originalData = chunk.toString();
+    const transformedData = originalData.split('').reverse().join('');
+    const dataToWrite = `--> ${transformedData} \n\n`;
+
+    callback(null, dataToWrite);
+  }
+});
+
 export const transform = async () => {
-    // Write your code here 
+  console.log('Привет!\nВведите текст, который хотите обработать');
+  console.log('(прим.: для выхода нажмите "ctrl + c"):\n');
+
+  try {
+    await pipeline(
+      stdin,
+      reverseText,
+      stdout
+    );
+  } catch (err) {
+    console.error(err);
+  }
 };
+
+transform();
