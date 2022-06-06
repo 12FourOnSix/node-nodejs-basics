d77b58c5a8da5a0693917bd12dd820978b282290
12FourOnSix <fouronsix@yandex.ru>
2022-06-06 13:32:53 +0300

    feat: receive data from main thread to worker, send solution back

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,15 @@
-
+import { Worker } from 'worker_threads';
 
 export const performCalculations = async () => {
-    // Write your code here
+  const 
+    worker = new Worker('./src/wt/worker.js'),
+    number = 7;
+
+  worker.postMessage(number);
+
+  worker.on('message', msg => {
+    console.log('Hi, it\'s main thread! \nThe result is: ', msg);
+  });
 };
+
+performCalculations();
